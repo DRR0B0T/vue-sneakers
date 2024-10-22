84388fafeb3592cdbf469ab4ece4f0c6d5fbd7cd
Ilmir  Ildarovich Mavliutov <mechasop@gmail.com>
2024-10-22 10:34:04 +0500

    vue sneakers

---
+++ src/components/CardList.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Card from './Card.vue'
+</script>
+
+<template>
+  <div class="grid grid-cols-4 gap-5">
+    <Card
+      :price="3500"
+      imageUrl="/sneakers/sneakers-1.jpg"
+      title="Мужские кроссовки Nike Blazer Mid Suede"
+    />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</template>
+
+<style scoped></style>
