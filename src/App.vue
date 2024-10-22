7ec706bab5646178c3922433fcdf36fd63e1edac
Ilmir  Ildarovich Mavliutov <mechasop@gmail.com>
2024-10-22 10:08:25 +0500

    vue sneakers

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
 import AppHeader from '@/components/AppHeader.vue'
+import ProductCard from '@/components/ProductCard.vue'
 </script>
 
 <template>
-  <div class="w-4/5 m-auto bg-white h-screen rounded-xl shadow-xl mt-14 mb-14">
+  <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14 mb-14 pb-5">
     <AppHeader />
-    <h1>Ilmir Mavliutov</h1>
+    <div>
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
   </div>
 </template>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <div
+    class="relative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
+  >
+    <img alt="Like 2" class="absolute top-8 left-8" src="/like-2.svg" />
+    <img alt="Sneakers" src="/sneakers/sneakers-1.jpg" />
+    <p class="mt-2">Мужские кроссовки Nike Blazer Mid Suede</p>
+    <div class="flex justify-between mt-5">
+      <div class="flex flex-col">
+        <span class="text-slate-400">Цена:</span>
+        <b>1205 руб.</b>
+      </div>
+      <img alt="Plus" src="/plus.svg" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
