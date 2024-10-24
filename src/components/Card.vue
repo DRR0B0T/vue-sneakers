f9cd559d2703d78d63926ff45884491d3519161d
Ilmir  Ildarovich Mavliutov <mechasop@gmail.com>
2024-10-24 13:37:09 +0500

    vue sneakers

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,6 +5,8 @@ defineProps({
   price: Number,
   isFavorite: Boolean,
   isAdded: Boolean,
+  onClickAdd: Function,
+  onFavoriteAdd: Function,
 })
 </script>
 
@@ -12,15 +14,27 @@ defineProps({
   <div
     class="bg-white relative border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
   >
-    <img alt="Like 2" class="absolute top-8 left-8" src="/like-2.svg" />
+    <img
+      :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
+      alt="Like 2"
+      class="absolute top-8 left-8"
+      @click="onFavoriteAdd"
+    />
+
     <img :src="imageUrl" alt="Sneakers" />
+
     <p class="mt-2">{{ title }}</p>
+
     <div class="flex justify-between mt-5">
       <div class="flex flex-col">
         <span class="text-slate-400">Цена:</span>
         <b>{{ price }} руб.</b>
       </div>
-      <img alt="Plus" src="/plus.svg" />
+      <img
+        :src="!isAdded ? ' /plus.svg' : '/checked.svg'"
+        alt="Plus"
+        @click="onClickAdd"
+      />
     </div>
   </div>
 </template>
--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,20 +1,25 @@
 <script setup>
 import Card from './Card.vue'
+
+const onFavoriteAdd = () => {
+  alert('Favorite Added')
+}
+const onClickAdd = () => {
+  alert('item Added')
+}
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
     <Card
+      :isAdded="false"
+      :isFavorite="false"
+      :onClickAdd="onClickAdd"
+      :onFavoriteAdd="onFavoriteAdd"
       :price="3500"
       imageUrl="/sneakers/sneakers-1.jpg"
       title="Мужские кроссовки Nike Blazer Mid Suede"
     />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
-    <Card />
   </div>
 </template>
 
