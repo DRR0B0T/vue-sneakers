54d0cd9bc04ee6b7579cd08165eb6602b6b15d2e
Ilmir  Ildarovich Mavliutov <mechasop@gmail.com>
2024-10-24 14:28:38 +0500

    vue sneakers

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import AppHeader from '@/components/AppHeader.vue'
 import CardList from '@/components/CardList.vue'
+import Drawer from '@/components/Drawer.vue'
 </script>
 
 <template>
   <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14 mb-14">
+    <Drawer />
     <AppHeader />
     <div class="p-10">
       <h2 class="text-4xl font-bold mb-8">Все кроссовки</h2>
