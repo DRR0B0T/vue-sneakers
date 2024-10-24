b80ee3992f044751a257d9ef424d444a981925e9
Ilmir  Ildarovich Mavliutov <mechasop@gmail.com>
2024-10-24 14:29:23 +0500

    vue sneakers

---
+++ src/components/CartItem.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <div class="flex items-center gap-4 border border-slate-100 p-4 rounded-xl">
+    <img alt="Sneakers" class="w-16 h-16" src="/sneakers/sneakers-1.jpg" />
+
+    <div class="flex flex-col justify-between">
+      <p>Кроссовки 5545dddsdfsdfsdfsdfsdf</p>
+      <div class="flex justify-between mt-2">
+        <b>5000 р.</b>
+        <img
+          alt="Close card"
+          class="opacity-40 hover:opacity-100 transition cursor-pointer"
+          src="/close.svg"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
