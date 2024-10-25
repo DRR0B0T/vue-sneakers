404d8d1d247ef12f7015850a458a221cad5e0373
Ilmir  Ildarovich Mavliutov <mechasop@gmail.com>
2024-10-25 10:12:14 +0500

    vue sneakers

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,35 @@
 <script setup>
 import AppHeader from '@/components/AppHeader.vue'
 import CardList from '@/components/CardList.vue'
-import Drawer from '@/components/Drawer.vue'
 </script>
 
 <template>
   <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14 mb-14">
-    <Drawer />
+    <!--    <Drawer />-->
     <AppHeader />
     <div class="p-10">
-      <h2 class="text-4xl font-bold mb-8">Все кроссовки</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-4xl font-bold mb-8">Все кроссовки</h2>
+
+        <div class="flex gap-4">
+          <select
+            class="py-2 px-3 border rounded-md outline-none cursor-pointer"
+          >
+            <option value="1">По названию</option>
+            <option value="2">По цене (дешёвые)</option>
+            <option value="3">По цене (дорогие)</option>
+          </select>
+
+          <div class="relative">
+            <img alt="Search" class="absolute left-4 top-3" src="/search.svg" />
+            <input
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              placeholder="Поиск..."
+              type="text"
+            />
+          </div>
+        </div>
+      </div>
       <CardList />
     </div>
   </div>
