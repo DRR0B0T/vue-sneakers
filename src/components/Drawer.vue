9f57214e0dbfc28a28eb89de3b76858338144f2b
Ilmir  Ildarovich Mavliutov <mechasop@gmail.com>
2024-11-01 09:39:14 +0500

    vue sneakers

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,6 +1,19 @@
 <script setup>
 import DrawerHeader from '@/components/DrawerHeader.vue'
 import CartListItem from '@/components/CartListItem.vue'
+import { computed } from 'vue'
+
+const emit = defineEmits(['createOrder'])
+
+const props = defineProps({
+  totalPrice: Number,
+  vatPrice: Number,
+  isCreatingOrder: Boolean,
+})
+
+const buttonDisabled = computed(
+  () => props.isCreatingOrder || !props.totalPrice,
+)
 </script>
 
 <template>
@@ -8,18 +21,25 @@ import CartListItem from '@/components/CartListItem.vue'
   <div class="bg-white h-full w-96 right-0 top-0 z-20 p-8 fixed">
     <DrawerHeader />
     <CartListItem />
-    <div>
+    <div class="flex flex-col gap-5 mb-5 mt-6">
       <div class="flex gap-2">
         <span>Итого:</span>
         <div class="flex-1 border-b border-dashed"></div>
-        <b>12900 р.</b>
+        <b>{{ totalPrice }} р.</b>
       </div>
 
       <div class="flex gap-2">
-        <span>Налог:</span>
+        <span>Налог 5%:</span>
         <div class="flex-1 border-b border-dashed"></div>
-        <b>5%</b>
+        <b>{{ vatPrice }} р.</b>
       </div>
+      <button
+        :disabled="buttonDisabled"
+        class="mt-4 w-full rounded-xl bg-lime-500 py-5 text-white active:bg-lime-700 hover:bg-lime-600 cursor-pointer transition disabled:bg-slate-500"
+        @click="emit('createOrder')"
+      >
+        Оформить заказ
+      </button>
     </div>
   </div>
 </template>
