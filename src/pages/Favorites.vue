f58b05e53ae657320e6c1c17f7205fcfb8dc7123
Ilmir  Ildarovich Mavliutov <mechasop@gmail.com>
2024-11-06 09:47:12 +0500

    vue sneakers

---
+++ src/pages/Favorites.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+const favorites = ref([])
+
+onMounted(async () => {
+  try {
+    const { data } = await axios.get(
+      `https://fd192f320b005090.mokky.dev/favorites`,
+    )
+  } catch (e) {
+    console.error(e)
+  }
+})
+</script>
+
+<template>
+  <h1>Favorites</h1>
+</template>
+
+<style scoped></style>
