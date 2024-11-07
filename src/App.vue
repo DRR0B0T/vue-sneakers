e6cb5b08f64e5ccf5792f7220fdcb701fe433d80
Ilmir  Ildarovich Mavliutov <mechasop@gmail.com>
2024-11-07 10:53:06 +0500

    vue sneakers

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,17 @@
 <script setup>
 import { computed, provide, ref, watch } from 'vue'
-import axios from 'axios'
 
 import AppHeader from '@/components/AppHeader.vue'
 import Drawer from '@/components/Drawer.vue'
 
 const cart = ref([])
 const drawerOpen = ref(false)
-const isCreatingOrder = ref(false)
 
 const totalPrice = computed(() =>
   cart.value.reduce((acc, item) => acc + item.price, 0),
 )
 
 const vatPrice = computed(() => Math.round((totalPrice.value * 5) / 100))
-
-const createOrder = async () => {
-  try {
-    isCreatingOrder.value = true
-    const { data } = await axios.post(
-      'https://fd192f320b005090.mokky.dev/orders',
-      {
-        items: cart.value,
-        totalPrice: totalPrice.value,
-      },
-    )
-
-    cart.value = []
-    return data
-  } catch (e) {
-    console.error(e)
-  } finally {
-    isCreatingOrder.value = false
-  }
-}
 
 const closeDrawer = () => {
   drawerOpen.value = false
@@ -71,17 +49,11 @@ provide('cartActions', {
 </script>
 
 <template>
-  <Drawer
-    v-if="drawerOpen"
-    :is-creating-order="isCreatingOrder"
-    :total-price="totalPrice"
-    :vat-price="vatPrice"
-    @create-order="createOrder"
-  />
+  <Drawer v-if="drawerOpen" :total-price="totalPrice" :vat-price="vatPrice" />
   <div class="w-4/5 m-auto bg-white rounded-xl shadow-xl mt-14 mb-14">
     <AppHeader :total-price="totalPrice" @open-drawer="openDrawer" />
     <div class="p-10">
-      <RouterView> </RouterView>
+      <RouterView />
     </div>
   </div>
 </template>
