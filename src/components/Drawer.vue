a41bbb706fa6952014c09034dfc720c721decd35
Ilmir  Ildarovich Mavliutov <mechasop@gmail.com>
2024-11-11 11:12:04 +0500

    vue sneakers

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,19 +1,42 @@
 <script setup>
 import DrawerHeader from '@/components/DrawerHeader.vue'
 import CartListItem from '@/components/CartListItem.vue'
-import { computed } from 'vue'
+import { computed, inject, ref } from 'vue'
 import Info from '@/components/Info.vue'
-
-const emit = defineEmits(['createOrder'])
+import axios from 'axios'
 
 const props = defineProps({
   totalPrice: Number,
   vatPrice: Number,
-  isCreatingOrder: Boolean,
 })
+const isCreatingOrder = ref(false)
+const orderItemId = ref(null)
+
+const { cart } = inject('cartActions')
+
+const createOrder = async () => {
+  try {
+    isCreatingOrder.value = true
+    const { data } = await axios.post(
+      'https://fd192f320b005090.mokky.dev/orders',
+      {
+        items: cart.value,
+        totalPrice: props.totalPrice,
+      },
+    )
+
+    cart.value = []
+    orderItemId.value = data.id
+    return data
+  } catch (e) {
+    console.error(e)
+  } finally {
+    isCreatingOrder.value = false
+  }
+}
 
 const buttonDisabled = computed(
-  () => props.isCreatingOrder || !props.totalPrice,
+  () => isCreatingOrder.value || !props.totalPrice,
 )
 </script>
 
@@ -21,13 +44,22 @@ const buttonDisabled = computed(
   <div class="fixed top-0 left-0 bg-black h-full w-full z-10 opacity-65"></div>
   <div class="bg-white h-full w-96 right-0 top-0 z-20 p-8 fixed">
     <DrawerHeader />
-    <div v-if="!totalPrice" class="flex h-full items-center">
+    <div v-if="!totalPrice || orderItemId" class="flex h-full items-center">
       <Info
+        v-if="orderItemId"
+        :description="`Ваш заказ №${orderItemId} скоро будет передан курьерской доставке.`"
+        image-url="/order-success-icon.png"
+        title="Заказ оформлен!"
+      />
+
+      <Info
+        v-if="!totalPrice && !orderItemId"
         description="Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ"
         image-url="/cart-empty.png"
         title="Корзина пустая"
       />
     </div>
+
     <div v-else>
       <CartListItem />
       <div class="flex flex-col gap-5 mb-5 mt-6">
@@ -45,7 +77,7 @@ const buttonDisabled = computed(
         <button
           :disabled="buttonDisabled"
           class="mt-4 w-full rounded-xl bg-lime-500 py-5 text-white active:bg-lime-700 hover:bg-lime-600 cursor-pointer transition disabled:bg-slate-500"
-          @click="emit('createOrder')"
+          @click="createOrder"
         >
           Оформить заказ
         </button>
