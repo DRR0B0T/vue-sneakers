0da1e74bbec835e7008bec54b164ded6e2f35db4
Ilmir  Ildarovich Mavliutov <mechasop@gmail.com>
2024-11-14 08:07:04 +0500

    vue sneakers

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -3,26 +3,23 @@ import Card from './Card.vue'
 
 defineProps({
   items: Array,
+  isFavorites: Boolean,
 })
 const emit = defineEmits(['onFavoriteAdd', 'onAddToCart'])
-
-const onClickAdd = () => {
-  alert('item Added')
-}
 </script>
 
 <template>
-  <div class="grid grid-cols-4 gap-5 mt-5">
+  <div v-auto-animate class="grid grid-cols-4 gap-5 mt-5">
     <Card
       v-for="item in items"
       :id="item.id"
       :key="item.id"
+      :if-favorites="isFavorites"
       :imageUrl="item.imageUrl"
       :isAdded="item.isAdded"
       :isFavorite="item.isFavorite"
-      :onAddToCart="() => emit('onAddToCart', item)"
-      :onClickAdd="onClickAdd"
-      :onClickFavorite="() => emit('onFavoriteAdd', item)"
+      :onAddToCart="isFavorites ? null : () => emit('onAddToCart', item)"
+      :onClickFavorite="isFavorites ? null : () => emit('onFavoriteAdd', item)"
       :price="item.price"
       :title="item.title"
     />
