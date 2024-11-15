6fb7d3ca2f1776337c6a78b99ce32df8724f0b57
Ilmir  Ildarovich Mavliutov <mechasop@gmail.com>
2024-11-15 07:07:33 +0500

    vue sneakers

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -17,6 +17,7 @@ defineProps({
     class="flex flex-col justify-between bg-white relative border border-slate-100 rounded-3xl p-8 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl"
   >
     <img
+      v-if="onClickFavorite"
       :src="!isFavorite ? '/like-1.svg' : '/like-2.svg'"
       alt="Like 2"
       class="absolute top-8 left-8"
@@ -33,6 +34,7 @@ defineProps({
         <b>{{ price }} ₽</b>
       </div>
       <img
+        v-if="onAddToCart"
         :src="!isAdded ? ' /plus.svg' : '/checked.svg'"
         alt="Plus"
         @click="onAddToCart"
